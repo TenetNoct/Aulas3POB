92aa623bb365794ec9fffd2c9446bcc04922bf78
Fellype Samuel <fellypesamuel1@gmail.com>
2025-02-27 14:20:28 -0300

    Exercício 4 - Cálculo de Média Simples

---
+++ aula1/exercicio4/e4.java
@@ -0,0 +1,28 @@
+package aula1.exercicio4;
+
+import java.util.Scanner;
+
+public class e4 {
+    public static void main(String[] args) {
+        
+        Scanner scanf = new Scanner(System.in);
+
+        System.out.println("Porfavor Digite a Primeira Nota:");
+
+        double nota1 = scanf.nextDouble();
+
+        System.out.println("Porfavor Digite a Segunda Nota:");
+
+        double nota2 = scanf.nextDouble();
+
+        System.out.println("Porfavor Digite a Terceira Nota:");
+
+        double nota3 = scanf.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A Média das Notas é:" + media);
+
+        scanf.close();
+    }
+}
