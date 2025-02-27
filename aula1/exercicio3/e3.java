502cc5bc9d1b8d3e62a72bf42aaa8704d426a664
Fellype Samuel <fellypesamuel1@gmail.com>
2025-02-27 14:20:04 -0300

    Exercício 3 -  Conversão de Temperatura

--- aula1/exercicio3/e3.java
+++ aula1/exercicio3/e3.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class e3 {
     public static void main(String[] args) {
+        
         Scanner scanf = new Scanner(System.in);
 
-        System.err.println("Porfavor Digite a temperatira em Celsius");
+        System.err.println("Porfavor Digite a temperatira em Celsius:");
         double celsius = scanf.nextDouble();
 
         double fahrenheit = (celsius * 9/5) + 32;
 
         System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
-
+        
+        scanf.close();
     }
 }
