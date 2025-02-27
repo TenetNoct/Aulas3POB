dece55eb1df68c31a7952dc71f5b8e857b126b73
Fellype Samuel <fellypesamuel1@gmail.com>
2025-02-27 14:26:39 -0300

    Exercício 3 - Correção de erro

--- aula1/exercicio3/e3.java
+++ aula1/exercicio3/e3.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class e3 {
     public static void main(String[] args) {
-        
+
         Scanner scanf = new Scanner(System.in);
 
-        System.err.println("Porfavor Digite a temperatira em Celsius:");
+        System.out.println("Porfavor Digite a temperatira em Celsius:");
         double celsius = scanf.nextDouble();
 
         double fahrenheit = (celsius * 9/5) + 32;
