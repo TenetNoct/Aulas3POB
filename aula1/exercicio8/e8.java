bc10130c6e42322f1beabb92bbc9b54767512eab
Fellype Samuel <fellypesamuel1@gmail.com>
2025-02-27 14:23:51 -0300

    Exercício 8 - Cálculo do Volume de uma Esfera

---
+++ aula1/exercicio8/e8.java
@@ -0,0 +1,21 @@
+package aula1.exercicio8;
+
+import java.util.Scanner;
+
+public class e8 {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        System.out.print("Insira o valor do raio da esfera: ");
+
+        final double  pi = 3.14159;
+
+        double raio = scanf.nextDouble();
+
+        double volume = (4.0/3) * pi * raio * raio * raio;
+
+        System.out.println("O volume da esfera é: " + volume);
+
+        scanf.close();
+    }
+}
