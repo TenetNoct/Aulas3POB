c091abebb42e50f3cb30b0b00c817b822fad3c64
Fellype Samuel <fellypesamuel1@gmail.com>
2025-03-25 20:56:27 -0300

    Projeto Pessoas

--- ProjetoPessoas/controle/controlaPessoa.java
+++ ProjetoPessoas/controle/controlaPessoa.java
@@ -1,3 +1,5 @@
+package controle;
+
 import dominio.Pessoa;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,6 +13,7 @@ public class controlaPessoa {
         while (opcao != 5) {
             System.out.println("1 - Cadastrar pessoa");
             System.out.println("2 - Listar pessoas");
+            System.out.println("3 - Calcular IMC");
             System.out.println("5 - Sair");
             opcao = scanf.nextInt();
 
@@ -31,6 +34,30 @@ public class controlaPessoa {
                 case 2:
                     for (Pessoa pessoa : pessoas) {
                         pessoa.ListarPessoa();
+                    }
+                    break;
+                case 3:
+                    if (pessoas.isEmpty()) {
+                        System.out.println("Não há pessoas cadastradas!");
+                    } else {
+                        System.out.println("\nLista de pessoas:");
+                        for (int i = 0; i < pessoas.size(); i++) {
+                            System.out.println(i + " - " + pessoas.get(i).getNome());
+                        }
+                        System.out.println("\nDigite o número da pessoa para calcular o IMC: ");
+                        int indice = scanf.nextInt();
+                        
+                        if (indice >= 0 && indice < pessoas.size()) {
+                            Pessoa pessoaSelecionada = pessoas.get(indice);
+                            double imc = pessoaSelecionada.calcularIMC();
+                            String interpretacao = pessoaSelecionada.interpretarIMC();
+                            
+                            System.out.println("\nNome: " + pessoaSelecionada.getNome());
+                            System.out.println("IMC: " + String.format("%.2f", imc));
+                            System.out.println("Classificação: " + interpretacao + "\n");
+                        } else {
+                            System.out.println("Índice inválido!");
+                        }
                     }
                     break;
                 case 5:
--- ProjetoPessoas/dominio/Pessoa.java
+++ ProjetoPessoas/dominio/Pessoa.java
@@ -62,5 +62,27 @@ public class Pessoa {
         System.out.println("Peso: " + this.getPeso());
         System.out.println("Sexo: " + this.getSexo() + "\n");
     }
+    
+    public double calcularIMC() {
+        return peso / (altura * altura);
+    }
+    
+    public String interpretarIMC() {
+        double imc = calcularIMC();
+        
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc < 25) {
+            return "Peso normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else if (imc < 35) {
+            return "Obesidade grau 1";
+        } else if (imc < 40) {
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grau 3";
+        }
+    }
 
 }
